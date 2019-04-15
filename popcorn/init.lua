0430f944955d23fa899e72833a05d0081315d48b
Cyber-Shinobi <49627860+Cyber-Shinobi@users.noreply.github.com>
2019-04-16 11:07:28 +1200

    Fixed bugs and added bag
    Update 0.2

--- popcorn/init.lua
+++ popcorn/init.lua
@@ -4,38 +4,38 @@ minetest.register_craftitem("popcorn:popcorn", {
 	on_use = minetest.item_eat(1),
 })
 
-minetest.register_craftitem("popcorn:popcornOP", {
-	description = "Popcorn! this is OP",
-	inventory_image = "popcorn_popcorn.png",
-	on_use = minetest.item_eat(10),
-})
+--minetest.register_craftitem("popcorn:popcornOP", {
+--	description = "Popcorn! this is OP",
+--	inventory_image = "popcorn_popcorn.png",
+--	on_use = minetest.item_eat(10),
+--})
 
 minetest.register_craft({
 	output = 'popcorn:popcorn',
 	recipe = {
 		{'', '', ''},
-		{'', 'default:dirt', ''},
-		{'', '', ''},
+		{'', 'farming:corn', ''},
+		{'', 'popcorn:bag', ''},
 	}
 })
 
-minetest.register_craft({
-	output = 'popcorn:popcorn_sword',
-	recipe = {
-		{'', 'default:dirt', 'default:dirt'},
-		{'', 'default:dirt', ''},
-		{'default:dirt', 'default:dirt', ''},
-	}
-})
+--minetest.register_craft({
+--	output = 'popcorn:popcorn_sword',
+--	recipe = {
+--		{'', 'default:dirt', 'default:dirt'},
+--		{'', 'default:dirt', ''},
+--		{'default:dirt', 'default:dirt', ''},
+--	}
+--})
 
-minetest.register_craft({
-	output = 'popcorn:popcornOP',
-	recipe = {
-		{'', '', ''},
-		{'', 'default:sand', ''},
-		{'', '', ''},
-	}
-})
+--minetest.register_craft({
+--	output = 'popcorn:popcornOP',
+--	recipe = {
+--		{'', '', ''},
+--		{'', 'default:sand', ''},
+--		{'', '', ''},
+--	}
+--})
 
 
 minetest.register_tool("popcorn:popcorn_sword", {
@@ -45,9 +45,9 @@ minetest.register_tool("popcorn:popcorn_sword", {
 		full_punch_interval = 0.7,
 		max_drop_level=1,
 		groupcaps={
-			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=40, maxlevel=3, not_in_creative_inventory = 1},
+			snappy={times={[1]=1.90, [2]=0.90, [3]=0.30}, uses=25, maxlevel=3, not_in_creative_inventory = 1},
 		},
-		damage_groups = {fleshy=8},
+		damage_groups = {fleshy=10},
 
 	},
 	sound = {breaks = "default_tool_breaks"},
